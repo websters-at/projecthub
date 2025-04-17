be8bef92916e37e2bdb6adb66ee449f89cdfbc44
Stevan Vlajic <91951603+stevan06v@users.noreply.github.com>
2025-04-17 16:10:33 +0200

    feat: deletion of todos possible now

--- app/Filament/Resources/TodosResource.php
+++ app/Filament/Resources/TodosResource.php
@@ -212,6 +212,8 @@ class TodosResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
